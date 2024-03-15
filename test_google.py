611f171cb17850db7ac890f8699ddc015baede1b
Ilya Sesyunin <ilia.sesiunin@samgau.com>
2024-03-15 12:36:52 +0500

    init commit

---
+++ test_google.py
@@ -0,0 +1,18 @@
+import pytest
+from selene import browser, be, have
+
+
+@pytest.fixture()
+def open_browser():
+    browser.driver.maximize_window()
+    browser.open('https://google.com/')
+
+
+def test_should_find(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_not_found(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('fdvjnhkfdeverufiofj4565262654738%^^&').press_enter()
+    browser.element('[class="card-section"]').should(have.text('ничего не найдено'))
